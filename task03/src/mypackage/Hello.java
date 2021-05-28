3bc92c900251f1977024e43b1e81714fa3b8a330
Nikita Golik <nikitq4a@gmail.com>
2021-05-28 21:09:44 +0300

    next task

---
+++ task03/src/mypackage/Hello.java
@@ -0,0 +1,18 @@
+package mypackage;
+import java.util.Scanner;
+
+/* Create a Hello class that will greet any user
+ * when entering its name via the command line.
+*/
+
+public class Hello {
+
+    public static void main(String[] args) {
+       
+    Scanner sc = new Scanner(System.in);
+	System.out.println("Enter your name");
+    String name = sc.next();
+    System.out.println("Hello, " + name);
+	sc.close();
+    }
+}
